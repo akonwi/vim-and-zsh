a5b5e66fcb1b1471056949b72e73ecb8bafca058
akonwi <akonwi@gmail.com>
2017-09-07 10:37:05 -0400

    zsh stuff

--- zsh/akonwi.plugin.zsh
+++ zsh/akonwi.plugin.zsh
@@ -12,20 +12,24 @@ alias myvim='vim ~/.vimrc'
 # Git stuff
 alias g="git"
 alias gad="git add --all ."
+alias gap="git add -p"
 alias gb="git branch"
 alias gc="git commit -m"
 alias gcm="git co master"
 alias gco="git co"
 alias gpu="git push"
 alias grb="git rebase"
+alias grbc="git rebase --continue"
+alias grba="git rebase --abort"
 alias gst="git status -sb"
 alias glog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
+# Maven
+alias mci="mvn clean install"
+alias mct="mvn clean test"
+
 # mocha
 alias mochacof="mocha --compilers coffee:coffee-script/register"
-
-# node-webkit launcher
-alias nw="~/Applications/node-webkit.app/Contents/MacOS/node-webkit"
 
 # My functions
 nodes() { cd ~/projects/node/$1; }
@@ -37,13 +41,3 @@ glone() {
   git clone "git@github.com:$1"
 }
 
-# build a node-webkit app, pass in the name of what to call it
-nw_build() {
-  name='app'
-  if [ "$1" ]
-  then
-    name=$1
-  fi
-  zip -r $name.nw *
-}
-compdef nw_build
--- zsh/akonwi.zsh-theme
+++ zsh/akonwi.zsh-theme
@@ -1,4 +1,4 @@
-PROMPT='%{$fg[cyan]%}%c$(git_prompt_info) %{$fg[red]%}» % %{$reset_color%}'
+PROMPT='%{$fg[cyan]%}%c$(git_prompt_info) %{$fg[red]%}❯ % %{$reset_color%}'
 # ➜
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}[%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}]"
