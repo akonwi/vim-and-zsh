96284a603d5a6bb428b6c732d0aed2d0408ff075
akonwi <akonwi@gmail.com>
2017-10-23 15:29:56 -0400

    add new abbreviated custom zsh file

---
+++ zsh/akonwi.zsh
@@ -0,0 +1,22 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+alias g="git"
+alias gst="git status -sb"
+alias gc="git commit -m"
+alias gcm="git co master"
+alias gco="git co"
+alias gad="git add --all ."
+alias gap="git add -p"
+alias gb="git branch"
+alias grb="git rebase"
+alias grbc="git rebase --continue"
+alias grba="git rebase --abort"
+alias glog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+# Git clone, pass in the repo as 'username/repo'
+glone() {
+  git clone "git@github.com:$1"
+}
